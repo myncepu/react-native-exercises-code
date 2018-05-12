10b94352e640e10e4f6c46d375a8c7ff96bace2b
Yan Pengqiang <yan2010@live.com>
2018-05-13 00:25:50 +0800

    Finish exercise1

--- Exercise1/index.js
+++ Exercise1/index.js
@@ -1,19 +1,42 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'gray',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 
-});
+  square: {
+    flexDirection: 'row',
+    backgroundColor: 'red',
+    width: 300,
+    height: 300,
+  },
+
+  blue: {
+    flex: 2,
+    backgroundColor: 'blue',
+  },
+
+  green: {
+    flex: 1,
+    backgroundColor: 'green',
+  },
+})
 
 const Exercise1 = () => {
   return (
-    <View>
-      <View>
-        <View />
-        <View />
+    <View style = { styles.container }>
+      <View style = { styles.square }>
+        <View style = { styles.blue }/>
+        <View style = { styles.green }/>
       </View>
     </View>
-  );
-};
+  )
+}
 
-export default Exercise1;
+export default Exercise1
